124ed8916e60c0edf6f59f7c697cab0cac3bb1f3
sas0mir <ssmirnov@i-novus.ru>
2024-11-11 23:51:06 +0100

    004: input-select component

--- src/routes/issues/index.tsx
+++ src/routes/issues/index.tsx
@@ -1,37 +1,25 @@
 import SearchInput from "../../components/search-input"
-import { ChangeEvent, useEffect } from "react"
+import { useEffect } from "react"
 import { useState } from "react"
 import { FaSearch } from "react-icons/fa"
-import axios, { AxiosResponse } from "axios"
 
 const IssuesPage = () => {
     const [username, setUsername] = useState('');
     const [repo, setRepo] = useState('');
-    const [data, setData] = useState([]);
+
+    const searchUserApi: string = `${import.meta.env.VITE_BACK_LOCATION}/app/search_user`;
+    const searchRepoApi: string = `${import.meta.env.VITE_BACK_LOCATION}/app/search_repo`;
 
     useEffect(() => {
-        async function searchUser(name: string) {
-            if (name.length > 2) {
-                const data: AxiosResponse = await axios.get(`${import.meta.env.VITE_BACK_LOCATION}/api/search_user`, {
-                    params: {
-                        name: name
-                    }
-                });
-                console.log('Data->', data);
-                setData(data.data)
-            }
-        }
-        searchUser(username)
     }, [username])
 
     return (
         <>
         <search className="m-6 block text-center container mx-auto lg:flex justify-center items-center">
-        <SearchInput title="username" handler={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)} value={username} />
+        <SearchInput title="username" api={searchUserApi} param="username" value={username} minlength={2} />
             <FaSearch />
-        <SearchInput title="repository" handler={(e: ChangeEvent<HTMLInputElement>) => setRepo(e.target.value)} value={repo} />
+        <SearchInput title="repository" api={searchRepoApi} param="repo" value={repo} minlength={1} />
         </search>
-        {data.length && data.map(el => <div>{el}</div>)}
         </>
     )
 }
